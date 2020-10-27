0a14c663c4751bd46bc54474cbffd48650913293
yangtao <1772492146@qq.com>
2020-10-27 19:25:21 +0800

    skin

--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58963" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63631" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
